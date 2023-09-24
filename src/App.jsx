d6682468209f0951234f311aaec0b0cf157b2384
alex@delcampoalacocina.com <alex@delcampoalacocina.com>
2023-09-24 21:57:17 +0200

    navbar y carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import "./App.css"
+import NavBar from "./components/NavBar";
+import Carousel from './components/Carousel'
+
 function App(){
-  return <h1>Hola chicos</h1>
+  return (
+  <div>
+    <NavBar/>
+    {
+
+    }
+    <Carousel/>
+    <div className="container">
+      <div className="row">
+        {
+
+        }
+      </div>
+    </div>
+  </div>
+  )
 }
 
 export default App;
