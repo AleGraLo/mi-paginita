8131be71380f7cfcdc25d812e105efefeedd2d58
alex@delcampoalacocina.com <alex@delcampoalacocina.com>
2023-09-24 23:56:27 +0200

    carousel coregido

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -6,46 +6,48 @@ import '../css/Carousel.css'
 
 
 const Carousel = () => {
-  return (
-    <div id="carouselExampleCaptions" className="carousel slide">
-  <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img src={imagen1} className="d-block w-100" alt="flor almendro"/>
-      <div className="carousel-caption text-dark p-3 bg-dark-subtle text-emphasis-dark rounded w-70 h-10">
-        <h5>Primavera en su Máxima Expresión</h5>
-        <p>Un hermoso almendro en plena floración, un regalo de la primavera.</p>
+    return (
+      <div id="carouselExampleCaptions" className="carousel slide">
+        <div className="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div className="carousel-inner container-carousel">
+          <div className="carousel-item active">
+            <img src={imagen1} className="d-block img-fluid w-100 h-100" alt="flor almendro" />
+            <div className="carousel-caption text-dark p-3 bg-dark-subtle text-emphasis-dark rounded">
+              <h5>Primavera en su Máxima Expresión</h5>
+              <p>Un hermoso almendro en plena floración, un regalo de la primavera.</p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src={imagen2} className="d-block img-fluid w-100 h-100" alt="almendras en árbol" />
+            <div className="carousel-caption text-dark p-3 bg-dark-subtle text-emphasis-dark rounded">
+              <h5>Cosecha a la Vista</h5>
+              <p>Una almendra madura se aferra al árbol, lista para ser recolectada en su mejor momento.</p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src={imagen3} className="d-block img-fluid w-100 h-100" alt="almendras agrupadas" />
+            <div className="carousel-caption text-dark p-3 bg-dark-subtle text-emphasis-dark rounded">
+              <h5>Abundancia Natural</h5>
+              <p>Una impresionante montaña de almendras recién recolectadas, la recompensa de una temporada de cosecha exitosa.</p>
+            </div>
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
-    </div>
-    <div className="carousel-item">
-      <img src={imagen2} className="d-block w-100" alt="almendras en arbol"/>
-      <div className="carousel-caption d-none d-md-block mb-3 text-dark p-3 mb-2 bg-dark-subtle text-emphasis-dark">
-        <h5>Cosecha a la Vista</h5>
-        <p>Una almendra madura se aferra al árbol, lista para ser recolectada en su mejor momento.</p>
-      </div>
-    </div>
-    <div className="carousel-item">
-      <img src={imagen3} className="d-block w-100" alt="almendras agrupadas"/>
-      <div className="carousel-caption d-none d-md-block mg-3 text-dark p-3 mb-2 bg-dark-subtle text-emphasis-dark">
-        <h5>Abundancia Natural</h5>
-        <p>Una impresionante montaña de almendras recién recolectadas, la recompensa de una temporada de cosecha exitosa.</p>
-      </div>
-    </div>
-  </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
-</div>
-  )
-}
+    )
+  }
+  
+  
 
 export default Carousel
